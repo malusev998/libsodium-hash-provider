9d39c71b109b5b644f9e5f6b34659f01d4e1cbf9
Dusan Malusev <dusan.998@outlook.com>
2020-03-08 17:44:48 +0100

    Adding config publishing and loading
    Signed-off-by: Dusan Malusev <dusan.998@outlook.com>

--- src/SodiumHashingProvider.php
+++ src/SodiumHashingProvider.php
@@ -8,10 +8,24 @@ use SodiumHashing\Manager\SodiumHashManager;
 
 class SodiumHashingProvider extends ServiceProvider
 {
+    protected const CONFIG = __DIR__ . '../config/sodium_hashing.php';
+
     public function register(): void
     {
+        $this->mergeConfigFrom(
+            self::CONFIG,
+            'sodium_hashing'
+        );
+
         $this->app->extend('hash', function (HashManager $manager, $app) {
             return new SodiumHashManager($manager, $app);
         });
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            self::CONFIG => config_path('sodium_hashing.php'),
+        ]);
     }
 }
